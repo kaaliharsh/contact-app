e6512ec893d003b3aef313c0343e85971a1cc19a
Harsh Deep Gupta <108045259+kaaliharsh@users.noreply.github.com>
2024-12-04 04:31:19 +0530

    user image done

--- src/components/contactCard.js
+++ src/components/contactCard.js
@@ -1,14 +1,18 @@
 import React  from "react";
-const ContactCards=()=>{
+import user from "../images/user.png"
+const ContactCard=(props)=>{
+    const{id,name,email}=props.contact;
     return(
         <div className="item">
+            <img className="ui avatar image" src={user} alt="user "></img>
         <div className="content">
-            <div className="header">{contact.name}</div>
-            <div className="header">{contact.email}</div>
+            <div className= "header">{name}</div>
+            <div className="header">{email}</div>
         </div>
         <i className="trash alternate outline icon" style={{color:"red", marginTop:"7px"}}></i>
         </div>
+
    
     );
 }
-export default ContactCards;
+export default ContactCard;
