1224da702cc502a0a7b6a677aeea50083d7e3ce9
Harsh Deep Gupta <108045259+kaaliharsh@users.noreply.github.com>
2024-12-04 05:30:01 +0530

    complete till the addContactHandler in AddContacts

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,18 +1,36 @@
 import React from "react";
 
 class AddContact extends  React.Component{
+    state={
+        name:"",
+        email:"",
+    };
+
+    add=(e)=>{
+        e.preventDefault();
+        if(this.state.name==="" || this.state.email===""){
+            alert("All the fills are manadatory!!!");
+            return;
+        }
+        this.props.addContactHandler(this.state);
+        this.setState({name:"",email:""});
+        // console.log(this.state);
+
+
+    }
+
     render(){
         return(
             <div className="ui main">
                 <h2>Add Contacts</h2>
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.add}>
                     <div className="field">
                         <label>Name: </label>
-                        <input type="text" name="name" placeholder="Your name"></input>
+                        <input type="text" name="name" placeholder="Your name" value={this.state.name} onChange={(e)=>this.setState({name:e.target.value})}></input>
                     </div>
                     <div className="field">
                         <label>Email: </label>
-                        <input type="text" name="name" placeholder="Your email"></input>
+                        <input type="text" name="email" placeholder="Your email" value={this.state.email} onChange={(e)=>this.setState({email:e.target.value})}/>
                     </div>
                     <button className="ui button blue">Add</button>
                 </form>
